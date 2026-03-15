0b474961d63a702996f9d3c641312833634a01da
gaelmunozavina <gaelmunozavina@gmail.com>
2026-03-14 20:20:55 -0600

    Create Seccion2-3_CursoPython.py

---
+++ Seccion2-3_CursoPython.py
@@ -0,0 +1,68 @@
+print("=== Python como calculadora ===")
+print(2 + 2)
+
+print("=== Operadores básicos ===")
+print(2 + 3)   # suma
+print(5 - 2)   # resta
+print(4 * 3)   # multiplicación
+print(6 / 3)   # división (siempre flotante)
+
+print("=== Exponenciación ===")
+print(2 ** 3)
+print(2 ** 3.)
+print(2. ** 3)
+print(2. ** 3.)
+
+print("=== Multiplicación ===")
+print(2 * 3)
+print(2 * 3.)
+print(2. * 3)
+print(2. * 3.)
+
+print("=== División ===")
+print(6 / 3)
+print(6 / 3.)
+print(6. / 3)
+print(6. / 3.)
+
+print("=== División entera ===")
+print(6 // 3)
+print(6 // 3.)
+print(6. // 3)
+print(6. // 3.)
+
+print(6 // 4)
+print(6. // 4)
+
+print(-6 // 4)
+print(6. // -4)
+
+print("=== Residuo (módulo) ===")
+print(14 % 4)
+print(12 % 4.5)
+
+print("=== Suma y resta ===")
+print(-4 + 4)
+print(-4. + 8)
+
+print("=== Operadores unarios ===")
+print(-1.1)
+print(+2)
+
+print("=== Prioridad de operadores ===")
+print(2 + 3 * 5)
+
+print("=== Enlace de operadores ===")
+print(9 % 6 % 2)
+
+print("=== Exponente con enlace derecho ===")
+print(2 ** 2 ** 3)
+
+print("=== Ejemplos con potencia ===")
+print(-3 ** 2)
+print(-2 ** 3)
+print(-(3 ** 2))
+
+print("=== Paréntesis ===")
+print((5 * ((25 % 13) + 100) / (2 * 13)))
+print(2 * 3 % 5)
