3181bfe9d648ab752638de4b48eb6a3cb53663c2
gaelmunozavina <gaelmunozavina@gmail.com>
2026-03-15 11:37:42 -0600

    Add input handling and display for user values

---
+++ Seccion4-1_CursoPython.py
@@ -0,0 +1,18 @@
+def message():
+    print("Por favor, ingresa un valor:")
+
+print("Inicio del programa")
+
+message()
+a = int(input())
+
+message()
+b = int(input())
+
+message()
+c = int(input())
+
+print("Los valores ingresados son:")
+print(a, b, c)
+
+print("Fin del programa")
